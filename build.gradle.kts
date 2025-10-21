45592c63f0cc71982d2817a2a87fbf58bf4bb51a
Jan Kroken <jan.kroken@nav.no>
2025-10-21 10:42:14 +0200

    gradle 9.1.0 + oppdaterte dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,42 +1,53 @@
+import com.github.benmanes.gradle.versions.updates.DependencyUpdatesTask
+import org.gradle.api.tasks.testing.logging.TestExceptionFormat.FULL
+import org.gradle.kotlin.dsl.withType
+import org.jetbrains.kotlin.gradle.dsl.JvmTarget
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
 group = "no.nav.pensjonopptjening"
 
+val junitJupiterVersion = "5.10.3"
+
 plugins {
-    kotlin("jvm") version "1.6.10"
+    val kotlinVersion = "2.2.10"
+    kotlin("jvm") version kotlinVersion
+    kotlin("plugin.serialization") version kotlinVersion
     `java-library`
     `maven-publish`
-    id("net.researchgate.release") version "2.8.1"
-}
-
-java {
-    toolchain {
-        languageVersion.set(JavaLanguageVersion.of(17))
-    }
+    id("net.researchgate.release") version "3.1.0"
+    id("com.github.ben-manes.versions") version "0.53.0"
 }
 
 repositories {
     mavenCentral()
 }
 
+java {
+    toolchain {
+        languageVersion.set(JavaLanguageVersion.of(21))
+    }
+}
+
 dependencies {
-    implementation("org.jetbrains.kotlin:kotlin-stdlib:1.6.10")
-    implementation("com.microsoft.azure:msal4j:1.11.3")
-    testImplementation("org.junit.jupiter:junit-jupiter:5.8.2")
+    implementation(kotlin("stdlib"))
+    implementation("com.microsoft.azure:msal4j:1.23.1")
+    testImplementation("org.junit.jupiter:junit-jupiter-api:${junitJupiterVersion}")
+    testImplementation("org.junit.jupiter:junit-jupiter-params:${junitJupiterVersion}")
+    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:${junitJupiterVersion}")
+    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
 }
 
 tasks.withType<Test> {
     useJUnitPlatform()
     testLogging {
         events("passed", "skipped", "failed")
-        exceptionFormat = org.gradle.api.tasks.testing.logging.TestExceptionFormat.FULL
+        exceptionFormat = FULL
     }
 }
 
-tasks {
-    withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile> {
-        kotlinOptions.jvmTarget = "17"
-    }
-    test {
-        useJUnitPlatform()
+tasks.withType<KotlinCompile> {
+    compilerOptions {
+        jvmTarget = JvmTarget.JVM_21
     }
 }
 
@@ -62,3 +73,14 @@ publishing {
         }
     }
 }
+
+tasks.withType<DependencyUpdatesTask>().configureEach {
+    rejectVersionIf {
+        isNonStableVersion(candidate.version)
+    }
+}
+
+fun isNonStableVersion(version: String): Boolean {
+    return listOf("BETA", "RC", "-M").any { version.uppercase().contains(it) }
+}
+
